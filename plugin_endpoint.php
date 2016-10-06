d42aa934d00c7e7ce02088359401168b42ba67e1
Joshua Woehlke <joshua.woehlke@gmail.com>
2016-10-06 10:29:42 -0500

    Comment corrections

--- plugin_endpoint.php
+++ plugin_endpoint.php
@@ -14,7 +14,7 @@
 $uploadPath = '/local/sendy/htdocs/sendyuploadfolder/';
 
 // The web-accessible path to your uploads folder (please include trailing slash)
-// (e.g. http://sendy.mst.edu/uploads, or https://email-p1.srv.sample.com/sendy/uploads)
+// (e.g. http://sendy.mst.edu/uploads/, or https://email-p1.srv.sample.com/sendy/uploads/)
 $visiblePath = 'http://yoursendyserver.com/uploads/';
 
 // That's it! User configuration done. Stop editing.
@@ -54,13 +54,13 @@ try {
 		throw new RuntimeException('Error: Unknown errors.');
 	}
 
-	// You should also check filesize here. 
+	// Verify filesize isn't gigantic
 	if ($_FILES['upfile']['size'] > 2500000) {
 		throw new RuntimeException('Error: Exceeded filesize limit (2.5MB).');
 	}
 
 	// DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
-	// Check MIME Type by yourself.
+	// Check MIME type manually
 	$finfo = new finfo(FILEINFO_MIME_TYPE);
 	if (false === $ext = array_search(
 		$finfo->file($_FILES['upfile']['tmp_name']),
@@ -72,11 +72,11 @@ try {
 		throw new RuntimeException('Error: Invalid file format.');
 	}
 
-	// You should name it uniquely.
-	// DO NOT USE $_FILES['upfile']['name'] WITHOUT ANY VALIDATION !!
-	// On this example, obtain safe unique name from its binary data.
+	// Get a unique name for this .zip in case it's uploaded multiple times
 	$fileName = sha1_file($_FILES['upfile']['tmp_name']) . '_' . rand(0, 99999999);
 	$filePath = $uploadPath . $fileName . '.' . $ext;
+
+	// Move the file into the uploads folder
 	if (!move_uploaded_file($_FILES['upfile']['tmp_name'], $filePath)) {
 		throw new RuntimeException('Error: Failed to handle uploaded file.');
 	}
@@ -84,20 +84,24 @@ try {
 	// Extract the uploaded zip archive
 	$zip = new ZipArchive();
 	if ($zip->open($filePath) === TRUE) {
-		// ONLY allow .html, .jpg, .png, .doc, .pdf, .csv, and .xls files in the zip.
+		// ONLY allow .html, .jpg, .png, .doc, .pdf, .csv, and .xls files in the zip. Attachments aren't handled anyway, but don't reject a .zip for having a common attachment.
 		for( $i = 0; $i < $zip->numFiles; $i++ ) {
 			$stat = $zip->statIndex( $i );
 			$name = basename( $stat['name'] );
-			trigger_error('NAME IS: ' . $name);
+			
+			// If this isn't a folder...
 			if ($stat['crc'] !== 0) {
 				if (!preg_match('/(\.jpg$|\.jpeg$|\.png$|\.html$|\.doc$|\.docx$|\.pdf$|\.xls$|\.xlsx$|\.csv$)/', $name)) {
 					throw new RuntimeException('Error: Not a valid BEE free email archive.');
 				}
+				// If this is the main .html file, grab its name for later
 				if (preg_match('/(\.html$)/', basename( $stat['name'] ))) {
 					$htmlDoc = basename( $stat['name'] );
 				}
 			}
 		}
+
+		// Create a directory to unzip to, then unzip
 		mkdir($uploadPath . $fileName);
 		$zip->extractTo($uploadPath . $fileName . '/');
 		$zip->close();
@@ -106,8 +110,13 @@ try {
 	}
 
 	if ($htmlDoc) {
+		// Get the contents of the .html file
 		$htmlDocString = file_get_contents($uploadPath . $fileName . '/' . $htmlDoc);
+
+		// Update the image paths to point to the right place
 		echo str_replace('src="images', 'src="' . $visiblePath . $fileName . '/images', $htmlDocString);
+
+		// Background images are currently hosted on AWS by BEE free. Would be a good modification to detect those and download them to the images folder, then replace those as well.
 	} else {
 		throw new RuntimeException('Error: Failed to retrieve HTML.');
 	}
